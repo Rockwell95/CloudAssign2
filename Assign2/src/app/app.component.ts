60cc7e850b3400daba40a07d4943d5933070d5d6
Rockwell95 <dominick.mancini@uoit.net>
2017-02-27 14:07:33 -0500

    Added tracker for AWS Analytics

--- Assign2/src/app/app.component.ts
+++ Assign2/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
 import {DataService} from "../providers/data-service";
 
+import * as AWS from 'aws-sdk';
+import * as AMA from 'aws-sdk-mobile-analytics'
 
 @Component({
   templateUrl: 'app.html'
@@ -19,6 +21,22 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+      //Make sure region is 'us-east-1'
+      AWS.config.region = 'us-east-1';
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-1:a12b7be8-b0bb-4d43-bfef-f35aa0ac7a17' //Amazon Cognito Identity Pool ID
+      });
+
+      let options = {
+        appId : 'c8181a9878d74edba8daf1c29deefdb3', //Amazon Mobile Analytics App ID
+        appTitle : 'CloudAssign2',              //Optional e.g. 'Example App'
+        appVersionName : 1.0, //Optional e.g. '1.4.1'
+        appVersionCode : 1, //Optional e.g. '42'
+        appPackageName : 'com.ionic.cloudassign2'  //Optional e.g. 'com.amazon.example'
+      };
+
+      let mobileAnalyticsClient = new AMA.Manager(options);
     });
   }
 }
